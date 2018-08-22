d65135bedd6711133c2733530fc1441e34c565fb
Agata Sumowska <agata.sumowska@gmail.com>
2018-08-22 21:34:59 +0100

    Happy place returns a json with the coldest, hottest and rainiest city

--- lib/happy_place.ex
+++ lib/happy_place.ex
@@ -1,6 +1,27 @@
 defmodule HappyPlace do
-
-  def hello do
-    :world
+  @default_city_ids_list [3088171, 2643743, 3117735, 703448, 2950159]
+  
+  defp create_list_of_city_weathers(city_ids_list \\ @default_city_ids_list, weather_api \\ OpenWeatherMapApi) do
+    Enum.map(city_ids_list, 
+      fn x -> 
+        CityWeather.read_weather(weather_api, x) 
+      end
+    )
+  end
+  
+  defp retrieve_result(map) do
+    {:ok, result} = map
+    result
+  end
+  
+  defp make_holiday_recommendation_json(city_weather_list) do
+    DestinationEvaluator.hottest_coldest_rainiest_map(city_weather_list)
+    |> Poison.encode
+    |> retrieve_result
+  end
+  
+  def make_holiday_recommendation do
+    create_list_of_city_weathers()
+    |>make_holiday_recommendation_json()
   end
 end
--- test/happy_place_test.exs
+++ test/happy_place_test.exs
@@ -2,7 +2,8 @@ defmodule HappyPlaceTest do
   use ExUnit.Case
   doctest HappyPlace
 
-  test "greets the world" do
-    assert HappyPlace.hello() == :world
+  test "returns a JSON with the hottest city" do
+    expected_json = "{\"rainiest_city\":{\"temperature\":292.84,\"name\":\"London\",\"id\":2643743,\"humidity\":68},\"hottest_city\":{\"temperature\":298.19,\"name\":\"Madrid\",\"id\":3117735,\"humidity\":41},\"coldest_city\":{\"temperature\":289.15,\"name\":\"Kiev\",\"id\":703448,\"humidity\":63}}"
+    assert expected_json ==  HappyPlace.make_holiday_recommendation
   end
 end
